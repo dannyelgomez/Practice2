b7fe334c3a0f65cc19dea737f7e8102f52990dab
Daniel Gomez <dannyelgomez@hotmail.com>
2020-12-16 20:44:03 -0500

    Table 16-12

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,25 @@
 const links = document.querySelectorAll('.link');
-links.forEach(link => {
-    link.addEventListener("click", selectView);
+
+links.forEach((link) => {
+    link.addEventListener('click', executeEvents);
 });
 
-function selectView(event) {
+function executeEvents(event) {
+    getList(event);
+    setSubmitEvent(event);
+}
+
+function setSubmitEvent(event) {
     const formName = event.currentTarget.getAttribute('data-form');
     setTimeout(() => {
         const form = document.getElementById(formName);
-        if (form) {
-            form.addEventListener("submit", handleFormSubmit);
-        }
-    }, 5000);
+        form.addEventListener("submit", handleFormSubmit);
+    }, 3000);
 }
-
-/* 1_users */
-/* const userForm = document.getElementById("user-form");
-userForm.addEventListener("submit", handleFormSubmit); */
-
-/* 2_regions */
-/* const regionForm = document.getElementById("region-form");
-regionForm.addEventListener("submit", handleFormSubmit); */
-
-/* 3_countries */
-/* const countryForm = document.getElementById("country-form");
-countryForm.addEventListener("submit", handleFormSubmit); */
-
-/* 4_contacts */
-/* const contactForm = document.getElementById("contact-form");
-contactForm.addEventListener("submit", handleFormSubmit); */
-
-/* 5_companies */
-/* const companyForm = document.getElementById("company-form");
-companyForm.addEventListener("submit", handleFormSubmit); */
-
-/* 6_cities */
-/* const cityForm = document.getElementById("city-form");
-cityForm.addEventListener("submit", handleFormSubmit); */
-
-/* categories */
-/* const categoriesForm = document.getElementById("categories-form");
-categoriesForm.addEventListener("submit", handleFormSubmit); */
 
 const testingForm = false;
 
 async function handleFormSubmit(event) {
-    debugger
     event.preventDefault();
 
     const form = event.currentTarget;
@@ -88,3 +63,55 @@ async function postFormDataAsJson({ url, formData }) {
 
     return response.text();
 }
+
+async function getList(event) {
+    // const formName = event.currentTarget.getAttribute('data-form');
+    const servicePath = event.currentTarget.getAttribute('data-service');
+
+    const fetchOptions = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        }
+    };
+
+    const response = await fetch(`http://127.0.0.1:3000/${servicePath}`, fetchOptions);
+    const rows = await response.json();
+
+    const title = 'User';
+    const headers = ['Id', 'User Name', 'Password', 'First Name', 'Last Name', 'Email', 'Role', 'Is Active'];
+
+    const tableString = createTable(title, headers, rows);
+
+    setTimeout(() => {
+        const currentList = document.getElementById("list");
+        currentList.innerHTML = tableString;
+    }, 2000);
+};
+
+function createTable(title, headers, rows) {
+    const fields = Object.keys(rows[0]);
+
+    const titleTemplate = `<caption>${title}</caption>`;
+    const tableHeadTemplate = `
+    <thead>
+      <tr>
+        ${headers.map(header => `<th scope="col">${header}</th>`).join(' ')}
+      </tr>
+    </thead>`;
+  
+    let tableBodyTemplate = `
+    <tbody>
+      ${rows.map(row => `<tr>${fields.map(field => `<td>${row[field]}</td>`).join(' ')}</tr>`).join(' ')}
+    <tbody>`;
+  
+  const tableTemplate = `
+    <table class="table caption-top">
+      ${titleTemplate}
+      ${tableHeadTemplate}
+      ${tableBodyTemplate}
+    </table>`;
+  
+  return tableTemplate;
+};
